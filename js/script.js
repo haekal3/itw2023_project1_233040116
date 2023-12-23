e052fbd3949a510f4a518f82043f3d5eed5d57a7
Haekal Khadafy <144595822+haekal3@users.noreply.github.com>
2023-12-23 13:22:14 +0700

    Menyempurnakan tugas besar

--- js/script.js
+++ js/script.js
@@ -7,16 +7,16 @@ menuIcon.addEventListener("click", () => {
 });
 
 // Contact Form //
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzNaey4Fd_Lzb_MFA01_j9ZPEbHXjYiGsVdqq8G6eSD9KgvwjNreCYiSqFHMa-kGjS0/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyFwKtLakhIIbwXXvtqXZsfXHBv_8EcIzI4JAfBLheEBNWCyaLt2x4bkfJEZAH1eLvC/exec'
 const form = document.forms['contact-form'];
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then((response) => {
-            // reset form
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            // reset
             form.reset();
             console.log('Success!', response);
         })
-        .catch(error => console.error('Error!', error.message));
+        .catch(error => console.error('Error!', error.message))
 });
